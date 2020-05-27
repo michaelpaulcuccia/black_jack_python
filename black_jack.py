f3b010e19ce2bbdc8ea6c20c853ef8dabe706a9d
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-05-27 16:59:09 -0400

    updated script

--- black_jack.py
+++ black_jack.py
@@ -3,8 +3,8 @@ import random
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
-                   'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
+          'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
 playing = True
 
@@ -25,6 +25,7 @@ class Deck():
         self.deck = []
         for suit in suits:
             for rank in ranks:
+                # using global vars, create a Card class, to append to deck list
                 self.deck.append(Card(suit, rank))
 
     def __str__(self):
@@ -42,4 +43,5 @@ class Deck():
 
 
 # test_deck = Deck()
+# test_deck.shuffle()
 # print(test_deck)
