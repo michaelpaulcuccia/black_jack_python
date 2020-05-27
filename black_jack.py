e2b49430466e70d8ed1df866141d57d8bf141279
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-05-27 14:27:57 -0400

    test_deck working

--- black_jack.py
+++ black_jack.py
@@ -5,3 +5,41 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
                    'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
+playing = True
+
+
+class Card():
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+
+class Deck():
+
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+
+    def __str__(self):
+        deck_composition = ''
+        for card in self.deck:
+            deck_composition += '\n' + card.__str__()
+        return 'The deck contains: ' + deck_composition
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+
+
+# test_deck = Deck()
+# print(test_deck)
