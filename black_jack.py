bb4bec896be5b84431fc1d2e7cd1d192795fadf5
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-05-28 09:21:12 -0400

    win, lose, bust functions

--- black_jack.py
+++ black_jack.py
@@ -96,7 +96,75 @@ class Chips():
         self.bet = 0
 
     def win_bet(self):
-        pass
+        self.total += self.bet
 
     def lose_bet(self):
-        pass
+        self.total -= self.bet
+
+
+def take_bet(chips):
+    while True:
+
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except:
+            print('Sorry, please provide a number')
+        else:
+            if chips.bet > chips.total:
+                print(
+                    f'Sorry, you do not have enough chips. You have {total.chips} chips.')
+            else:
+                break
+
+
+def hit(deck, hand):
+    single_card = deck.deal()
+    hand.add_card(single_card)
+    hand.adjust_for_ace()
+
+
+def hit_or_stand(deck, hand):
+    global playing
+
+    while True:
+        x = input('Hit or Stand? Enter h or s.')
+        if x[0].lower() == 'h':
+            hit(deck, hand)
+        elif x[0].lower() == 's':
+            print('Player Stands. Dealer\'s turn.)
+            playing = False
+        else:
+            print('Please enter h or s only.')
+            continue
+
+        break
+
+
+def player_busts(player, dealer, chips):
+    print('BUST! Player loses.')
+    chips.lose_bet()
+
+
+def player_wins(player, dealer, chips):
+    print('Player Wins!!!')
+    chips.win_bet()
+
+
+def dealer_busts(player, dealer, chips)
+
+
+print('Player Wins! Dealer busts.')
+chips.win_bet()
+
+
+def dealer_wins(player, dealer, chips)
+
+
+print('Dealer Wins!!!')
+chips.lose_bet()
+
+
+def push(player, dealer)
+
+
+print('Dealer and Player tie. PUSH!')
